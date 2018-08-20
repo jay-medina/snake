70e36a14b2581a9a41f336ddd92c6b9b3088464a
Jose Medina <josemedina921@gmail.com>
2018-08-19 17:33:18 -0700

    creating the score component

--- elm_version/src/Main.elm
+++ elm_version/src/Main.elm
@@ -1,8 +1,48 @@
 module Main exposing (..)
 
-import Html exposing (text)
+import Components.Score exposing (scoreboard)
+import Html exposing (Html, div, program, text)
+import Html.Attributes exposing (class)
 
 
-main : Html.Html msg
+type Msg
+    = Nothing
+
+
+type alias Model =
+    String
+
+
+init : ( Model, Cmd Msg )
+init =
+    ( "hello", Cmd.none )
+
+
+view : Model -> Html Msg
+view model =
+    div [ class "snake__app" ]
+        [ scoreboard { current = 20, highScore = 25 }
+        , text model
+        ]
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        Nothing ->
+            ( model, Cmd.none )
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.none
+
+
+main : Program Never Model Msg
 main =
-    text "hello"
+    Html.program
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        }
