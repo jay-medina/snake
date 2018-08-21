dfc7f0cf2331ee4f384b128cd92890e1f7ede9eb
Jose Medina <josemedina921@gmail.com>
2018-08-21 09:34:01 -0700

    upgrading to elm 19 and removing webpack

--- elm_version/src/Main.elm
+++ elm_version/src/Main.elm
@@ -5,6 +5,39 @@ import Model exposing (Model)
 import Msg exposing (Msg)
 
 
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.batch
+        [ if model.gameState == Run then
+            every model.timer Tick
+          else
+            Sub.none
+        ]
+
+
 main : Program Never Model Msg
 main =
-    createApp { row = 25, col = 25 }
+    let
+        initModel : Model
+        initModel =
+            { score = 0
+            , highScore = 20
+            , timer = 200
+            , row = 25
+            , col = 25
+            , apple = initialApple
+            , snake = initialSnake
+            , gameState = Start
+            , direction = Right
+            }
+
+        init : ( Model, Cmd Msg )
+        init =
+            ( initModel, Cmd.none )
+    in
+        Html.program
+            { init = init
+            , view = screen
+            , update = update
+            , subscriptions = subscriptions
+            }
