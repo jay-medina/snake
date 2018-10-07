9635906df28ed13db1e1b42c0d35b21fb92485d2
Jose Medina <josemedina921@gmail.com>
2018-10-07 13:28:49 -0700

    Add it capitalized

---
+++ ts_redux/src/components/scoreboard/ScoreBoard.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+export interface ScoreBoardProps {
+  score: number;
+  highScore: number;
+}
+
+interface ScoreProps {
+  title: string;
+  num: number;
+}
+
+const Score: React.StatelessComponent<ScoreProps> = ({ title, num }) => (
+  <div className="snake__score">
+    <div className="snake__score-title">{title}</div>
+    <div className="snake__score-number">{num}</div>
+  </div>
+);
+
+export const ScoreBoard: React.SFC<ScoreBoardProps> = ({ score, highScore }) => (
+  <div className="snake__scoreboard">
+    <Score title="score" num={score} />
+    <Score title="high" num={highScore} />
+  </div>
+);
+ScoreBoard.displayName = 'Scoreboard';
