e6ada25d5a28c03909a9bc796da71603497984ec
Jose Medina <josemedina921@gmail.com>
2018-10-07 18:28:02 -0700

    adding an app test

---
+++ ts_redux/src/components/app.test.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createGame } from './app';
+import { Paint } from './paint/Paint';
+
+jest.mock('react-dom', () => ({
+  render: jest.fn(),
+}));
+
+describe('createGame', () => {
+  beforeEach(() => {
+    const root = document.createElement('div');
+    root.id = 'root';
+    document.body.appendChild(root);
+
+    createGame();
+  });
+
+  it('paints the game to the DOM node', () => {
+    expect(ReactDOM.render).toHaveBeenCalledWith(<Paint />, document.getElementById('root'));
+  });
+});
