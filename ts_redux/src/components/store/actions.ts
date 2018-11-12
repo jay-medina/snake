e63f370c3b29a79504b62325ef4611dd39179bc9
Jose Medina <josemedina921@gmail.com>
2018-11-11 16:15:07 -0800

    Hook up Thunk Actions

--- ts_redux/src/components/store/actions.ts
+++ ts_redux/src/components/store/actions.ts
@@ -1,3 +1,6 @@
+// import { GameThunkAction } from './types';
+// import { AppState } from './util';
+
 interface GameAction {
   type: 'START_GAME';
 }
@@ -7,3 +10,13 @@ export type AppAction = GameAction;
 export const startGame = (): GameAction => ({
   type: 'START_GAME',
 });
+
+// export function randomizeApple(appState: AppState) {
+
+// }
+
+// export const startGameThunk = (): GameThunkAction => (dispatch) => {
+//   // randomize the apple position
+//   // start the game
+// //  return dispatch(startGame);
+// };
--- ts_redux/src/components/store/gameStore.ts
+++ ts_redux/src/components/store/gameStore.ts
@@ -1,6 +1,7 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 import { game } from './reducer';
 
 export function createGameStore() {
-  return createStore(game);
+  return createStore(game, applyMiddleware(thunk));
 }
