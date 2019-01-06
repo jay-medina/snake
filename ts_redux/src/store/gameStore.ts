53ee04842015bbbddb4f87ba29eaeab9cf4ca321
Jose Medina <josemedina921@gmail.com>
2019-01-05 20:15:58 -0800

    moving state to the reducer

--- ts_redux/src/store/gameStore.ts
+++ ts_redux/src/store/gameStore.ts
@@ -1,20 +1,7 @@
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
-import { AppState, GameState } from './util';
 import { app } from './reducer';
 
-const initialState: AppState = {
-  dimensions: {
-    cols: 25,
-    rows: 25,
-  },
-  apple: {
-    row: 4,
-    col: 4,
-  },
-  gameState: GameState.Start,
-};
-
 export function createGameStore() {
-  return createStore(app, initialState, applyMiddleware(thunk));
+  return createStore(app, applyMiddleware(thunk));
 }
--- ts_redux/src/store/reducer.ts
+++ ts_redux/src/store/reducer.ts
@@ -1,6 +1,17 @@
 import { AppAction } from './actions';
 import { GameState, Apple, AppState } from './util';
-import { getInitialState } from './initialState';
+
+const initialState = {
+  dimensions: {
+    cols: 25,
+    rows: 25,
+  },
+  apple: {
+    row: 2,
+    col: 2,
+  },
+  gameState: GameState.Start,
+};
 
 const gameState = (state: GameState, action: AppAction): GameState => {
   if (action.type === 'START_GAME') {
@@ -14,7 +25,7 @@ const apple = (state: Apple, _action: AppAction): Apple => {
   return state;
 };
 
-export const app = (state: AppState = getInitialState(), action: AppAction): AppState => {
+export const app = (state: AppState = initialState, action: AppAction): AppState => {
   return {
     ...state,
     apple: apple(state.apple, action),
