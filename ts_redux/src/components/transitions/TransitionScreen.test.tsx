5f8656c6cd0ebf8db6e72aad4408404c83b1128c
Jose Medina <josemedina921@gmail.com>
2019-01-06 13:12:10 -0800

    update transition screen test

--- ts_redux/src/components/transitions/TransitionScreen.test.tsx
+++ ts_redux/src/components/transitions/TransitionScreen.test.tsx
@@ -2,17 +2,16 @@ jest.mock('../../common/util');
 
 import { mount, ReactWrapper } from 'enzyme';
 import React from 'react';
-import { GameState, AppState, Apple } from '../../common/types';
+import { GameState, AppState } from '../../common/types';
 import TransitionScreen from './TransitionScreen';
 import { Provider } from 'react-redux';
-import { startGame, newApplePosition } from '../../store/actions';
+import { startGame, startGameThunk } from '../../store/actions';
 import {
   createMockState,
   createMockStore,
   StoreCreator,
   MockStore,
 } from '../../common/testhelpers';
-import { findNewApplePosition } from '../../common/util';
 
 type noop = () => void;
 
@@ -28,7 +27,6 @@ describe('<TransitionScreen />', () => {
   });
 
   describe('when the game is running', () => {
-
     beforeEach(() => {
       store = storeCreator({
         ...mockState,
@@ -70,23 +68,17 @@ describe('<TransitionScreen />', () => {
     });
 
     describe('when user clicks play', () => {
-      let newApple: Apple;
-
       beforeEach(() => {
-        newApple = { row: 10, col: 10 };
+        (startGameThunk as jest.Mock) = jest.fn(startGame);
 
-        (findNewApplePosition as jest.Mock).mockReturnValue(newApple);
         const onPlayClick = wrapper.find('Start').prop<noop>('onPlayClick');
 
         onPlayClick();
       });
 
-      it('dispatches the start game action', () => {
+      it('dispatches the start game thunk action', () => {
+        expect(startGameThunk).toHaveBeenCalled();
         expect(store.getActions()).toContainEqual(startGame());
-      });
-
-      it('dispatches update apple action', () => {
-        expect(store.getActions()).toContainEqual(newApplePosition(newApple));
       });
     });
   });
@@ -110,23 +102,17 @@ describe('<TransitionScreen />', () => {
     });
 
     describe('when user clicks play', () => {
-      let newApple: Apple;
-
       beforeEach(() => {
-        newApple = { row: 10, col: 10 };
+        (startGameThunk as jest.Mock) = jest.fn(startGame);
 
-        (findNewApplePosition as jest.Mock).mockReturnValue(newApple);
         const onPlayClick = wrapper.find('GameOver').prop<noop>('onPlayClick');
 
         onPlayClick();
       });
 
-      it('dispatches the start game action', () => {
+      it('dispatches the start game thunk action', () => {
+        expect(startGameThunk).toHaveBeenCalled();
         expect(store.getActions()).toContainEqual(startGame());
-      });
-
-      it('dispatches update apple action', () => {
-        expect(store.getActions()).toContainEqual(newApplePosition(newApple));
       });
     });
   });
