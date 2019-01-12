6a2413a655b13c424219f8f8f5073c85c72eff3d
Jose Medina <josemedina921@gmail.com>
2019-01-11 21:38:08 -0800

    update app

--- ts_redux/src/app.tsx
+++ ts_redux/src/app.tsx
@@ -1,21 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { Paint } from './components/paint/Paint';
-import { createGameStore } from './store/gameStore';
-import { startLoop } from './components/app/gameloop';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import { Paint } from './components/paint/Paint'
+import { createGameStore } from './store/gameStore'
+import { startLoop } from './components/app/gameloop'
 
 export function createGame() {
-  const store = createGameStore();
+  const store = createGameStore()
 
   const Game = (
     <Provider store={store}>
       <Paint />
     </Provider>
-  );
+  )
 
-  ReactDOM.render(Game, document.getElementById('root'));
+  ReactDOM.render(Game, document.getElementById('root'))
 
-  startLoop(store);
+  startLoop(store)
 }
-
