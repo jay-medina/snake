8e15e8dbe8d99624736c0a297bde8a2c0c36a66f
Jose Medina <josemedina921@gmail.com>
2019-02-17 11:44:57 -0800

    start over

--- elm_version/src/Main.elm
+++ elm_version/src/Main.elm
@@ -1,24 +1,7 @@
-module Main exposing (..)
+module Main exposing (main)
 
-import Browser
-import Model exposing (Model, GameState(..), Direction(..))
-import Msg exposing (Msg)
-import Util exposing (initialModel)
-import Views.Screen exposing (screen)
-import Update exposing (update)
-import Subscriptions exposing (subscriptions)
+import Html exposing (..)
 
 
-main : Program Int Model Msg
 main =
-    let
-        init : Int -> ( Model, Cmd Msg )
-        init flags =
-            ( initialModel, Cmd.none )
-    in
-        Browser.element
-            { init = init
-            , view = screen
-            , update = update
-            , subscriptions = subscriptions
-            }
+    text "Hello"
