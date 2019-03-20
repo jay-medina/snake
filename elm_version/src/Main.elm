ffbe331c28750ed74c23125354b5b494f9f4482f
Jose Medina <josemedina921@gmail.com>
2019-03-19 21:17:49 -0700

    Refactoring the playing area to its own component

--- elm_version/src/Main.elm
+++ elm_version/src/Main.elm
@@ -1,29 +1,14 @@
 module Main exposing (main)
 
 import Browser
-import Browser.Events exposing (onAnimationFrame, onKeyDown)
-import Html exposing (..)
-import Html.Attributes exposing (..)
-import Json.Decode as Decode
 import Subscriptions exposing (..)
-import Types exposing (Direction(..), GameState(..), Model, Msg(..))
+import Types exposing (Model, Msg)
 import Update exposing (init, update)
-import Views.Board exposing (board)
-import Views.Scoreboard exposing (scoreboard)
-import Views.TransitionScreen exposing (transitionScreen)
+import Views.Playingarea exposing (playingarea)
 
 
 
 -- Main View
-
-
-playingarea : Model -> Html Msg
-playingarea model =
-    div []
-        [ scoreboard model
-        , board model
-        , transitionScreen model
-        ]
 
 
 initForMain : () -> ( Model, Cmd Msg )
